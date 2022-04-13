3cce931cba1a4121c2a0fd4751ccf557ec7a1367
Danyal Zia <danyal6870@gmail.com>
2022-04-13 05:33:55 +0500

    Add MIT license and update project configuration files

--- robustify/functional.py
+++ robustify/functional.py
@@ -1,22 +1,18 @@
-# Copyright (c) 2021-present, GYU Co., Ltd. All rights reserved.
+# Copyright (c) 2022-present, All rights reserved.
 
 # Author: Danyal Zia Khan
 # Email: danyal6870@gmail.com
 # Copyright (c) 2020-2022 Danyal Zia Khan
 # All rights reserved.
 
-# USE OF THIS SOFTWARE AND DISTRIBUTION OUTSIDE THE GYU COMPANY IS STRICTLY PROHIBITED. PLEASE REPORT ANY SUCH USE TO THE COMPANY.
-
 from __future__ import annotations
 
 import asyncio
-
 from functools import cache
 from typing import TYPE_CHECKING, Awaitable, Generic, ParamSpec, TypeVar, overload
 
 from robustify.error import MaxTriesReached
 from robustify.result import Err, Ok
-
 
 if TYPE_CHECKING:
     from typing import Callable, Iterable
--- robustify/result.py
+++ robustify/result.py
@@ -1,19 +1,15 @@
-# Copyright (c) 2021-present, GYU Co., Ltd. All rights reserved.
+# Copyright (c) 2022-present, All rights reserved.
 
 # Author: Danyal Zia Khan
 # Email: danyal6870@gmail.com
 # Copyright (c) 2020-2022 Danyal Zia Khan
 # All rights reserved.
 
-# USE OF THIS SOFTWARE AND DISTRIBUTION OUTSIDE THE GYU COMPANY IS STRICTLY PROHIBITED. PLEASE REPORT ANY SUCH USE TO THE COMPANY.
-
 from __future__ import annotations
 
 import functools
 import inspect
-
 from typing import TYPE_CHECKING, Any, Generic, TypeVar, cast
-
 
 if TYPE_CHECKING:
     from typing import (
