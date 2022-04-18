3cd67680f9f9f44a5bb52fd7ba8a81a1f978d69a
Danyal Zia <danyal6870@gmail.com>
2022-04-18 09:45:51 +0500

    Change version number to default 0.1.0; add imports to __init__.py

--- robustify/__init__.py
+++ robustify/__init__.py
@@ -1,1 +1,26 @@
-__version__ = "0.1.2"
+from robustify.functional import do, isin
+from robustify.result import (
+    Err,
+    Ok,
+    Result,
+    UnwrapError,
+    assert_never,
+    is_err,
+    is_ok,
+    returns,
+    returns_future,
+)
+
+__all__ = [
+    "Ok",
+    "Err",
+    "Result",
+    "returns",
+    "returns_future",
+    "UnwrapError",
+    "is_ok",
+    "is_err",
+    "assert_never",
+    "do",
+    "isin",
+]
