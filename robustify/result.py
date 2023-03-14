e2866cb1a162293a98e6591173de58b9aa62a2cc
Danyal Zia <danyal6870@gmail.com>
2023-03-14 06:42:57 +0500

    Update deps; update license text

--- robustify/result.py
+++ robustify/result.py
@@ -1,9 +1,24 @@
-# Copyright (c) 2022-present, All rights reserved.
+# MIT License
 
-# Author: Danyal Zia Khan
-# Email: danyal6870@gmail.com
-# Copyright (c) 2020-2022 Danyal Zia Khan
-# All rights reserved.
+# Copyright (c) 2022 Danyal Zia Khan
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from __future__ import annotations
 
@@ -12,17 +27,8 @@ import inspect
 from typing import TYPE_CHECKING, Any, Generic, TypeVar, cast
 
 if TYPE_CHECKING:
-    from typing import (
-        TYPE_CHECKING,
-        Awaitable,
-        Callable,
-        Final,
-        NoReturn,
-        ParamSpec,
-        TypeGuard,
-    )
-
-    from typing_extensions import Never
+    from collections.abc import Awaitable, Callable
+    from typing import TYPE_CHECKING, Final, NoReturn, ParamSpec, TypeGuard
 
     U = TypeVar("U")
     TBE = TypeVar("TBE", bound=BaseException)
@@ -283,10 +289,3 @@ def is_err(val: Result[ReturnType, E]) -> TypeGuard[ReturnType]:
     Shorthand for isinstance(val, Err)
     """
     return isinstance(val, Err)
-
-
-def assert_never(__arg: Never) -> Never:
-    """
-    When using structural pattern matching, exhaustively check the type in last case statement
-    """
-    raise AssertionError("Expected code to be unreachable")
